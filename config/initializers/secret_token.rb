8de593f14de89f0f3e214d15bcf7ce97e82c30ee
U-QuadVista64\Nik <Nik@QuadVista64.(none)>
2011-02-10 23:26:06 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '2a995998ac8ed694a87f24e8328109807cb5cf0de0049cc23b1784a7cfefd7b471e2b4a649a37a519f4aec79f86db93e1728b081db0d039baeda4405eb4ec435'
